956591b28b35a4ec6a9cc642702c75367e599787
Boyinkus <ebaltaza20@jaumebalmes.net>
2021-04-27 16:09:34 +0200

    DE LOKOS

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Món");
-        System.out.println("");
+        int n1 = 1 ;
+        int n2 = 2 ;
+        System.out.println("El resultado de la resta es: " resultado(n1,n2));
+
+    }
+    public static int resultado (int n1, int n2)  {
+        return n1 - n2 ;
     }
 
 }
