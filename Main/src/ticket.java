6fa333362a104dbc2664b8a90a14e8670bdbbf4e
oseznoRaudo <jordiandrade14@gmail.com>
2021-05-04 16:29:31 +0200

    autor jordi

--- Main/src/ticket.java
+++ Main/src/ticket.java
@@ -6,7 +6,30 @@ Este Archivo contendrá el ticket
  */
 
 public class ticket {
+
+
+
+
+
+    public static void ventaprincipal(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Si quieres volver al apartado principal presiona 1:");
+        System.out.println("Si quiere continuar con la elección ventas presione 2:");
+        int n2=sc.nextInt();
+
+
+        switch (n2) {
+            case 1: Main.opcionesprincipales();
+                break;
+            case 2: Ventas();
+        }
+
+    }
 public static void Ventas () {
+    System.out.println("has elegido la opción Ventas:");
+    System.out.println("Que producto quieres?");
+
+
 
 }
 }
